c1f66b5b0fe8869547ca2c98f75bbf304c0366af
jmata188 <37884863+jmata188@users.noreply.github.com>
2018-03-28 18:38:20 -0400

    Update category.php
    Added other categories

--- category.php
+++ category.php
@@ -5,8 +5,13 @@ include('Menu.php');
 <div class="categorys_page">
 <a href="toys.php"><button>Toys</button></a>
 <a href="electronic.php"><button>Electronic</button></a>
-<a href=""><button>Car Accessories</button></a>
-<a href=""><button>IPhones</button></a>
+<a href="videoGame.php"><button>Video Games</button></a>
+<a href="dvds.php"><button>DVDs</button></a>
+<a href="books.php"><button>Books</button></a>
+<a href="music.php"><button>Music</button></a>
+<a href="officeSupplies.php"><button>Office Supplies</button></a>
+<a href="tools.php"><button>Tools</button></a>
+<!--
 <a href=""><button>none</button></a>
 <a href=""><button>none</button></a>
 <a href=""><button>none</button></a>
@@ -19,9 +24,6 @@ include('Menu.php');
 <a href=""><button>none</button></a>
 <a href=""><button>none</button></a>
 <a href=""><button>none</button></a>
-<a href=""><button>none</button></a>
-<a href=""><button>none</button></a>
-<a href=""><button>none</button></a>
-<a href=""><button>none</button></a>
+-->
 
 </div>
